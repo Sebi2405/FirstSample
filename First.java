b5a7802bace21c163092140de50456adc8a72d1a
Sebi2405 <sebigerman@yahoo.com>
2018-06-26 15:37:59 +0300

    primul

---
+++ First.java
@@ -0,0 +1,19 @@
+public class First {
+    private int nr;
+
+    public First(int id) {
+        this.nr = id;
+    }
+
+    public int getId() {
+        return nr;
+    }
+
+    public void setId(int id) {
+        this.nr = id;
+    }
+
+    public static void main(String[] args) {
+        System.out.println("mdsada das;dk");
+    }
+}
